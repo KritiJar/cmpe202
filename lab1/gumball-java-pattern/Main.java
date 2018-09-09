ec1b25b5e3e1508ff5c3e4b80650564a00352f81
KritiJar <42920978+KritiJar@users.noreply.github.com>
2018-09-08 19:30:33 -0700

    Update Main.java

--- lab1/gumball-java-pattern/Main.java
+++ lab1/gumball-java-pattern/Main.java
@@ -1,1 +1,37 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+        OneQuarterMachine gumballMachine1 = new OneQuarterMachine(5);
+        TwoQuarterMachine gumballMachine2=new TwoQuarterMachine(5);
+        AllCoinMachine gumballMachine3=new AllCoinMachine(5);
+
+        System.out.println(gumballMachine1);
+        gumballMachine1.insertCoin(10);
+        gumballMachine1.insertCoin(25);
+        gumballMachine1.turnCrank();
+        
+    	System.out.println(gumballMachine2);
+
+        gumballMachine2.insertCoin(25);
+        gumballMachine2.turnCrank();
+        gumballMachine2.insertCoin(25);
+        gumballMachine2.turnCrank();
+
+
+        
+      	System.out.println(gumballMachine3);
+        
+        gumballMachine3.insertCoin(25);
+        gumballMachine3.turnCrank();
+        gumballMachine3.insertCoin(10);
+        gumballMachine3.turnCrank();
+        
+        gumballMachine3.insertCoin(10);
+        gumballMachine3.turnCrank();
+        gumballMachine3.turnCrank();
+        
+        gumballMachine3.insertCoin(5);
+        gumballMachine3.turnCrank();
+    }
+}
