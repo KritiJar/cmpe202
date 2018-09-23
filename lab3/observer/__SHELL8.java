199ac164c859ff02aef6d94ac28da3b1acf1447b
KritiJar <42920978+KritiJar@users.noreply.github.com>
2018-09-22 22:07:54 -0700

    Add files via upload

---
+++ lab3/observer/__SHELL8.java
@@ -0,0 +1,12 @@
+package observer;
+public class __SHELL8 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\Kriti\\Desktop\\CMPE_202\\Java\\observer\\observer");
+final observer.ObserverTest observer1 = (observer.ObserverTest)__bluej_runtime_scope.get("observer1");
+
+final observer.ObserverTest observer2 = (observer.ObserverTest)__bluej_runtime_scope.get("observer2");
+
+
+observer2.runTest();
+
+}}
