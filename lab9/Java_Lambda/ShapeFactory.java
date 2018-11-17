1f10181c6b9c9a5f0d271bb657395118f8a38566
KritiJar <42920978+KritiJar@users.noreply.github.com>
2018-11-17 13:29:45 -0800

    Added Lab 9

---
+++ lab9/Java_Lambda/ShapeFactory.java
@@ -0,0 +1,14 @@
+public class ShapeFactory {
+  final static Map<String, Supplier<Shape>> map = new HashMap<>();
+  static {
+    map.put("CIRCLE", Circle::new);
+    map.put("RECTANGLE", Rectangle::new);
+  }   
+  public Shape getShape(String shapeType){
+     Supplier<Shape> shape = map.get(shapeType.toUpperCase());
+     if(shape != null) {
+       return shape.get();
+     }
+     throw new IllegalArgumentException("No such shape " + shapeType.toUpperCase());
+  }
+}
